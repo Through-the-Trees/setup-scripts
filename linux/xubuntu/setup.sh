f3c898233c6b9e96514ba73dbe9e9ab1fbd3a14b
DanielRubianes <daniel@throughthetrees.us>
2025-10-10 22:08:28 -0400

    re-map keys last (restart baked into script)

--- linux/xubuntu/setup.sh
+++ linux/xubuntu/setup.sh
@@ -7,14 +7,13 @@ yes | sudo apt install git-all
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-
-sudo git clone https://github.com/Lioncat6/remap-util /chromebook-keys-remap
-cd /chromebook-keys-remap
-sudo bash remap-util.sh
-sudo rm -rf /chromebook-keys-remap
-
 sudo apt install libreoffice
 CONFIG_PATH="$HOME/.config/libreoffice/4/user"
 mkdir -p "$CONFIG_PATH"
 
 curl -o "$CONFIG_PATH/registrymodifications.xcu" "https://raw.githubusercontent.com/Through-the-Trees/setup-scripts/refs/heads/main/linux/libreoffice-config/registrymodifications.xcu"
+
+sudo git clone https://github.com/Lioncat6/remap-util /chromebook-keys-remap
+cd /chromebook-keys-remap
+sudo bash remap-util.sh
+sudo rm -rf /chromebook-keys-remap
