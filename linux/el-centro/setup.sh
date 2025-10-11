a9960b242d0a30e5a5f142ab22187a6e863a940f
DanielRubianes <drubianes1352@gmail.com>
2025-10-11 14:58:25 -0400

    el centro linux setup

---
+++ linux/el-centro/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update base packages
+sudo apt update && apt upgrade && apt autoremove
+
+# Install Chrome
+cd ~
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i ./google-chrome-stable_current_amd64.deb
+sudo rm -rf ./google-chrome-stable_current_amd64.deb
+
+# Install Firefox
+
+# Install Libre office and copy registrymodifications.xcu
+sudo apt install libreoffice
+CONFIG_PATH="$HOME/.config/libreoffice/4/user"
+mkdir -p "$CONFIG_PATH"
+curl -o "$CONFIG_PATH/registrymodifications.xcu" "https://raw.githubusercontent.com/Through-the-Trees/setup-scripts/refs/heads/main/linux/libreoffice-config/registrymodifications.xcu"
