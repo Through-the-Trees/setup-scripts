ed2f4830042f2e5ba2de7af721f9be6387b12659
DanielRubianes <daniel@throughthetrees.us>
2025-10-10 22:16:13 -0400

    temp comment

--- linux/xubuntu/setup.sh
+++ linux/xubuntu/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo apt update -Y && apt upgrade -Y && apt autoremove -Y
-
+sudo apt update && apt upgrade && apt autoremove
 sudo apt install curl
+sudo apt install git-all
 
-yes | sudo apt install git-all
+cd ~
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i ./google-chrome-stable_current_amd64.deb
@@ -16,7 +16,7 @@ mkdir -p "$CONFIG_PATH"
 
 curl -o "$CONFIG_PATH/registrymodifications.xcu" "https://raw.githubusercontent.com/Through-the-Trees/setup-scripts/refs/heads/main/linux/libreoffice-config/registrymodifications.xcu"
 
-sudo git clone https://github.com/Lioncat6/remap-util /chromebook-keys-remap
-cd /chromebook-keys-remap
-sudo bash remap-util.sh
-sudo rm -rf /chromebook-keys-remap
+# sudo git clone https://github.com/Lioncat6/remap-util /chromebook-keys-remap
+# cd /chromebook-keys-remap
+# sudo bash remap-util.sh
+# sudo rm -rf /chromebook-keys-remap
