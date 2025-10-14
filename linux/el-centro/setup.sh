418eb628c56ce8e921fee117536e67621df9ead5
DanielRubianes <daniel@throughthetrees.us>
2025-10-14 18:57:17 -0400

    f

--- linux/el-centro/setup.sh
+++ linux/el-centro/setup.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt upgrade
 # Install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i ./google-chrome-stable_current_amd64.deb
-sudo rm ./google-chrome-stable_current_amd64.deb
+sudo rm -f ./google-chrome-stable_current_amd64.deb
 
 # Install Libre office and copy registrymodifications.xcu
 sudo apt install libreoffice -Y
