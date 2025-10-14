25043b93befafaadc13ce24ce4028beb3deee5b1
DanielRubianes <daniel@throughthetrees.us>
2025-10-14 18:57:00 -0400

    rm -rf

--- linux/el-centro/setup.sh
+++ linux/el-centro/setup.sh
@@ -1,54 +1,27 @@
 #!/bin/bash
 
 # Update base packages
-sudo dnf update
+sudo apt update && sudo apt upgrade
 
 # Install Chrome
-sudo dnf install chrome-stable -Y
-sudo dnf install firefox -Y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i ./google-chrome-stable_current_amd64.deb
+sudo rm ./google-chrome-stable_current_amd64.deb
 
 # Install Libre office and copy registrymodifications.xcu
-sudo dnf install libreoffice -Y
+sudo apt install libreoffice -Y
 CONFIG_PATH="$HOME/.config/libreoffice/4/user"
 mkdir -p "$CONFIG_PATH"
 curl -o "$CONFIG_PATH/registrymodifications.xcu" "https://raw.githubusercontent.com/Through-the-Trees/setup-scripts/refs/heads/main/linux/libreoffice-config/registrymodifications.xcu"
 
+# Disable window animations for performance optimization
+kwriteconfig6 --key AnimationDurationFactor 0
+
 # Keyboard setup
 echo "
-running cros-keyboard-map by WeirdTreeThing...
-
-BSD 3-Clause License
-
-Copyright (c) 2023, WeirdTreeThing
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-   contributors may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE U"
-
-
+running cros-keyboard-map script by WeirdTreeThing...
+(https://github.com/WeirdTreeThing/cros-keyboard-map)
+"
 git clone https://github.com/WeirdTreeThing/cros-keyboard-map ~/keyd-chromebook
-
 sudo bash ~/keyd-chromebook/install.sh
-
 rm -rf ~/keyd-chromebook
-
--- linux/xubuntu/setup.sh
+++ linux/xubuntu/setup.sh
@@ -6,7 +6,7 @@ cd ~
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i ./google-chrome-stable_current_amd64.deb
-sudo rm -rf ./google-chrome-stable_current_amd64.deb
+sudo rm ./google-chrome-stable_current_amd64.deb
 
 sudo apt install libreoffice
 CONFIG_PATH="$HOME/.config/libreoffice/4/user"
