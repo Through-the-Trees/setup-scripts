0ba5c442cf046f58a8e0adf494f60c49736483e5
DanielRubianes <daniel@throughthetrees.us>
2025-10-14 23:09:53 -0400

    Fedora again

--- linux/el-centro/setup.sh
+++ linux/el-centro/setup.sh
@@ -4,12 +4,17 @@
 sudo apt update && sudo apt upgrade
 
 # Install Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i ./google-chrome-stable_current_amd64.deb
-sudo rm -f ./google-chrome-stable_current_amd64.deb
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i ./google-chrome-stable_current_amd64.deb
+# sudo rm -f ./google-chrome-stable_current_amd64.deb
+
+# Install Chrome in Fedora
+sudo dnf install fedora-workstation-repositories
+sudo dnf config-manager setopt google-chrome.enabled=1
+sudo dnf install google-chrome-stable
 
 # Install Libre office and copy registrymodifications.xcu
-sudo apt install libreoffice -Y
+sudo dnf install libreoffice -Y
 CONFIG_PATH="$HOME/.config/libreoffice/4/user"
 mkdir -p "$CONFIG_PATH"
 curl -o "$CONFIG_PATH/registrymodifications.xcu" "https://raw.githubusercontent.com/Through-the-Trees/setup-scripts/refs/heads/main/linux/libreoffice-config/registrymodifications.xcu"
@@ -25,6 +30,6 @@ echo "
 running cros-keyboard-map script by WeirdTreeThing...
 (https://github.com/WeirdTreeThing/cros-keyboard-map)
 "
-git clone https://github.com/WeirdTreeThing/cros-keyboard-map ~/keyd-chromebook
+sudo git clone https://github.com/WeirdTreeThing/cros-keyboard-map ~/keyd-chromebook
 sudo bash ~/keyd-chromebook/install.sh
-rm -rf ~/keyd-chromebook
+sudo rm -rf ~/keyd-chromebook
