c6bdabb059f76517a80b4632cf4822cdc8ba0db5
DanielRubianes <drubianes1352@gmail.com>
2025-10-15 22:09:15 -0400

    config6

--- linux/el-centro/setup.sh
+++ linux/el-centro/setup.sh
@@ -17,13 +17,16 @@ CONFIG_PATH="$HOME/.config/libreoffice/4/user"
 mkdir -p "$CONFIG_PATH"
 curl -o "$CONFIG_PATH/registrymodifications.xcu" "https://raw.githubusercontent.com/Through-the-Trees/setup-scripts/refs/heads/main/linux/libreoffice-config/registrymodifications.xcu"
 
+echo "Configuring KDE Desktop...."
 # Pin apps to "Task Manager"
 launcher_apps="applications:google-chrome.desktop,applications:org.mozilla.firefox.desktop,preferred://filemanager,applications:org.kde.discover.desktop"
-kwriteconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group "Containments" --group "2" --group "Applets" --group "5" --group "Configuration" --group "General" --key "launchers" "$launcher_apps"
-
+kwriteconfig6 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group "Containments" --group "2" --group "Applets" --group "5" --group "Configuration" --group "General" --key "launchers" "$launcher_apps"
 # Set Background
 path_to_wallpaper="/usr/share/wallpapers/Altai"
-kwriteconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group "Containments" --group "1" --group "Wallpaper" --group "org.kde.image" --group "General" --key "Image" "$path_to_wallpaper"
+kwriteconfig6 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group "Containments" --group "1" --group "Wallpaper" --group "org.kde.image" --group "General" --key "Image" "$path_to_wallpaper"
+
+echo "Applying KDE Desktop configuration..."
+systemctl --user restart plasma-plasmashell
 
 # Rename device
 read -p "Enter device asset tag: " asset
