0005d63e2f672d808a45df7e58cd5a4bafad2ffa
DanielRubianes <drubianes1352@gmail.com>
2025-10-15 21:35:04 -0400

    KDE Configuration

--- linux/el-centro/setup.sh
+++ linux/el-centro/setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Install Chrome
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo dpkg -i ./google-chrome-stable_current_amd64.deb
-# sudo rm -f ./google-chrome-stable_current_amd64.deb
+# Disable window animations for performance optimization
+kwriteconfig6 --key AnimationDurationFactor 0
 
 # Install Chrome in Fedora
-sudo dnf install fedora-workstation-repositories
+echo "Installing Google Chrome..."
+sudo dnf install fedora-workstation-repositories -Y
 sudo dnf config-manager setopt google-chrome.enabled=1
-sudo dnf install google-chrome-stable
+sudo dnf install google-chrome-stable -Y
 
 # Install Libre office and copy registrymodifications.xcu
+echo "Installing Libre Office..."
 sudo dnf install libreoffice -Y
+echo "Configuring Libre Office..."
 CONFIG_PATH="$HOME/.config/libreoffice/4/user"
 mkdir -p "$CONFIG_PATH"
 curl -o "$CONFIG_PATH/registrymodifications.xcu" "https://raw.githubusercontent.com/Through-the-Trees/setup-scripts/refs/heads/main/linux/libreoffice-config/registrymodifications.xcu"
@@ -19,5 +20,21 @@ curl -o "$CONFIG_PATH/registrymodifications.xcu" "https://raw.githubusercontent.
 # ~/.config/plasma-org.kde.plasma.desktop-appletsrc
 # [Containments][2][Applets][5][Configuration][General]
 
+# Background
+path_to_wallpaper = '/usr/share/wallpapers/Altai'                            # The path to the wallpaper.
+kwriteconfig5                                                    \ # The configuration tool.
+  --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" \ # The path to the configuration file.
+    --group 'Containments'                                       \
+      --group '1'                                                \
+        --group 'Wallpaper'                                      \ # This can, alternatively, be a colour.
+          --group 'org.kde.image'                                \
+            --group 'General'                                    \
+              --key 'Image' "$path_to_wallpaper"                   # The key which contains the path to the wallpaper.
+
+# Rename device
+read -p "Enter device asset tag: " asset
+sudo hostnamectl set-hostname 'TtT-$asset'
+
 # Update base packages
-sudo dnf update && sudno dnf upgrade
+# echo "Updating base packages..."
+# sudo dnf update && sudno dnf upgrade
