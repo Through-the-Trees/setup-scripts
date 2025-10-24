020a89db50d434529b71cebb4bc2d82b5a5c84c6
DanielRubianes <drubianes1352@gmail.com>
2025-10-24 15:46:16 -0400

    Zorin setup

--- linux/zorin/setup.sh
+++ linux/zorin/setup.sh
@@ -1,35 +1,21 @@
 #! /usr/bin/env bash
 
-# Check Wi-Fi connection status
-if nmcli -t -f WIFI g | grep -q "enabled"; then
-    if ! (nmcli -t -f ACTIVE,SSID dev wifi | grep -q "^yes"); then
-        echo "Connect to wifi before running script."
-        exit 1
-    fi
-else
-    echo "Connect to wifi before running script."
-    exit 1
-fi
+echo "Removing Brave Browser..."
+sudo apt autoremove -y --purge brave-browser
 
-# sudo apt autoremove -y --purge brave-browser
-
+echo "Installing software..."
 flatpak install -y flathub com.google.Chrome
 flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub org.videolan.VLC
 
 # Apply configurations
-
-# Libre Office default file formats
-#USB_PATH=$(findmnt -n -o TARGET --target \
-#	"$(sudo find /media/* -type d -name '\$OEM\$' -print -quit)")
-
-
+echo "Configuring Libre Office..."
 CONFIG_PATH="$HOME/.config/libreoffice/4/user"
 mkdir -p "$CONFIG_PATH"
-cd $CONFIG_PATH && curl 
-cp "$USB_PATH/\$OEM\$/\$1/TtT Setup/registrymodifications.xcu" "$CONFIG_PATH/registrymodifications.xcu"
+curl -o "$CONFIG_PATH/registrymodifications.xcu" "https://raw.githubusercontent.com/Through-the-Trees/setup-scripts/refs/heads/main/linux/libreoffice-config/registrymodifications.xcu"
 
 # Set VLC as default for common media types
+echo "Configuring VLC..."
 xdg-mime default org.videolan.VLC.desktop video/mp4
 xdg-mime default org.videolan.VLC.desktop video/x-matroska
 xdg-mime default org.videolan.VLC.desktop video/x-msvideo
@@ -40,9 +26,15 @@ xdg-mime default org.videolan.VLC.desktop audio/mp4
 xdg-mime default org.videolan.VLC.desktop application/ogg
 
 # Set firefox as default PDF & Browser
+echo "setting default browser..."
 xdg-settings set default-web-browser org.mozilla.firefox.desktop
 xdg-mime default org.mozilla.firefox.desktop application/pdf
 
 # Set power settings to keep screen on
+echo "Adjusting power settings for shop display..."
 gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 gsettings set org.gnome.desktop.session idle-delay 0
+
+# Pin apps to taskbar
+echo "Pinning apps to taskbar..."
+gsettings set org.gnome.shell favorite-apps "['org.mozilla.firefox.desktop', 'com.google.Chrome.desktop', 'org.gnome.Nautilus.desktop', 'libreoffice-startcenter.desktop']"
