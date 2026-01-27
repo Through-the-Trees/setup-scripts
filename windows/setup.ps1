2fb5ca6b8a6e34ade4cea45427964673800fa913
DanielRubianes <drubianes1352@gmail.com>
2026-01-26 19:27:24 -0500

    roll back

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,7 +1,7 @@
-if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
-    Exit
-}
+# if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+#     Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
+#     Exit
+# }
 
 $remote = "https://raw.githubusercontent.com/Through-the-Trees/setup-scripts/master"
 
